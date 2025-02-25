7f3adc9d752cb404d24ffff9169ff7c5f23c6ff0
hendisantika <hendisantika@yahoo.co.id>
2025-02-25 10:23:07 +0700

    feat: Modify Post Livewire🫸🌀✏️📗🐧🐳⬆

--- app/Livewire/Post.php
+++ app/Livewire/Post.php
@@ -6,6 +6,11 @@ use Livewire\Component;
 
 class Post extends Component
 {
+    /**
+     * define public variable
+     */
+    public $title, $content, $postId, $slug, $status, $updatePost = false, $addPost = false;
+
     public function render()
     {
         return view('livewire.post');
